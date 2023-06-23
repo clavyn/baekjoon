8ff0e0827580c38b6dc92f0fb14f89f5b3940105
clabyn <hn06127@naver.com>
2023-06-23 15:33:46 +0900

    0623

--- baekjoon/src/baekjoon/Main.java
+++ baekjoon/src/baekjoon/Main.java
@@ -1,54 +1,49 @@
 package baekjoon;
 
-import java.util.Arrays;
+import java.util.Stack;
 
 public class Main {
 
-	static int[] solution(int n, int m) {
-		boolean flag = false;
-		int max = 0;//최대공약수
-		int min = 0;//최소공배수
-		if(n>m) {
-			int t = n ;
-			n = m;
-			m=t;
-		}		
-		if((m/(double)n)%1==0) {
-			max=n;
-			min=m;
+	static int solution(int n) {
+		int answer = 0;
+		boolean flag = true;
+		String bin = "";
+		int idx = n + 1;
+		bin = Integer.toBinaryString(n);
+		char[] arr = bin.toCharArray();
+		int oneCnt = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == '1')
+				oneCnt++;
 		}
 		
-		//최대공약수 구하기
-		for(int i = 0 ; i <m ;i++) {
-			if((((double)n/i)%1==0)&&(((double)m/i)%1==0)) {
-				max=i;
+		String tmp = "";
+		char[] temp;
+		int cnt = 0;
+		while (flag) {
+			tmp = Integer.toBinaryString(idx);
+			temp = tmp.toCharArray();
+			cnt=0;
+			for (int i = 0; i < temp.length; i++) {
+				if (temp[i] == '1')
+					cnt++;
 			}
-		}
-		//최소공배수 구하기
-		int tmp=0;
-		int mul = 0;
-		for(int i = 1; i<m;i++) {
-			tmp = m*i;
-			for(int j = 1 ; j <tmp ; j++) {			
-				if(tmp==mul) {
-					min = tmp;
-					flag=true;
-					break;
-				}
-				mul=n*j;
+			
+			if (cnt == oneCnt) {
+				answer = idx;
+				flag = false;
+				break;
 			}
-			if(flag) break;
+			idx++;
 		}
-		
-		
-		
-		int [] answer = {max,min};
+
 		return answer;
 
 	}
 
 	public static void main(String[] args) {
-		System.out.println(Arrays.toString(solution(3,12)));
-		System.out.println(Arrays.toString(solution(2,5)));
+		System.out.println(solution(78));// 83
+		System.out.println(solution(15));// 23
+
 	}
 }
