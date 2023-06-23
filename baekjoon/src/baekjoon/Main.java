fe8fedfc3f0171fda1a720336ec56c9f47073e98
clabyn <hn06127@naver.com>
2023-06-23 17:37:26 +0900

    0623

--- baekjoon/src/baekjoon/Main.java
+++ baekjoon/src/baekjoon/Main.java
@@ -3,18 +3,30 @@ package baekjoon;
 
 public class Main {
 
-	static int solution(int[] number) {
-		int answer = 0 ;
-		
-		
+	static String solution(String s, int n) {
+		String answer="";
+		int origin = n;
+		char [] arr = s.toCharArray();
+		for(int i = 0; i< arr.length ; i++) {
+			n=origin;
+			if(arr[i]==' ') {
+				answer+= ' ';
+				continue;
+				}
+			if(Character.isUpperCase(arr[i])) {
+				answer+=(char)((arr[i]-'A'+n)%26 + 'A');
+				
+			}else if(Character.isLowerCase(arr[i])) {
+				answer+=(char)((arr[i]-'a'+n)%26 + 'a');
+			}
+		}
 		
 		return answer;
 
 	}
 
 	public static void main(String[] args) {
-		int[] number = {2,3,0,2,-5};
-		System.out.println(solution(number));
+		System.out.println(solution("AB",1));
 
 
 	}
