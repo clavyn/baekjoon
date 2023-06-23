2b28bafdfbc25100d312f2a99d80fb0acedbee7c
clabyn <hn06127@naver.com>
2023-06-23 16:21:58 +0900

    0623

--- baekjoon/src/baekjoon/Main.java
+++ baekjoon/src/baekjoon/Main.java
@@ -1,49 +1,21 @@
 package baekjoon;
 
-import java.util.Stack;
 
 public class Main {
 
-	static int solution(int n) {
-		int answer = 0;
-		boolean flag = true;
-		String bin = "";
-		int idx = n + 1;
-		bin = Integer.toBinaryString(n);
-		char[] arr = bin.toCharArray();
-		int oneCnt = 0;
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] == '1')
-				oneCnt++;
-		}
+	static int solution(int[] number) {
+		int answer = 0 ;
 		
-		String tmp = "";
-		char[] temp;
-		int cnt = 0;
-		while (flag) {
-			tmp = Integer.toBinaryString(idx);
-			temp = tmp.toCharArray();
-			cnt=0;
-			for (int i = 0; i < temp.length; i++) {
-				if (temp[i] == '1')
-					cnt++;
-			}
-			
-			if (cnt == oneCnt) {
-				answer = idx;
-				flag = false;
-				break;
-			}
-			idx++;
-		}
-
+		
+		
 		return answer;
 
 	}
 
 	public static void main(String[] args) {
-		System.out.println(solution(78));// 83
-		System.out.println(solution(15));// 23
+		int[] number = {2,3,0,2,-5};
+		System.out.println(solution(number));
+
 
 	}
 }
